b623a801ebe4df178cf3f0641aeba798fdca0ea6
so-kaushal <kaushal.chauhan@oncehub.com>
2025-03-17 10:36:18 +0530

    chore: ONCEHUB-94992 Upgrade Angular app to version 19. update Angular dependencies and version to 5.0.0-beta.6

--- src/lib/knowledge-owl-link/knowledge-owl-link.spec.ts
+++ src/lib/knowledge-owl-link/knowledge-owl-link.spec.ts
@@ -4,16 +4,18 @@ import { KnowledgeOwlLinkModule } from './index';
 import { getKnowledgeLinkArticleMissingError } from './knowledge-owl-widget-errors';
 
 @Component({
-  template: `<a knowledgeOwlLink>Test link</a>`,
+    template: `<a knowledgeOwlLink>Test link</a>`,
+    standalone: false
 })
 class KnowledgeOwlLinkWithoutArticle {}
 
 @Component({
-  template: `
+    template: `
     <a knowledgeOwlLink="https://knowledgeowl.article.com/help/test-article"
       >Test link</a
     >
   `,
+    standalone: false
 })
 class KnowledgeOwlLinkWithValidInputs {}
 
--- src/lib/knowledge-owl-widget/knowledge-owl-widget.ts
+++ src/lib/knowledge-owl-widget/knowledge-owl-widget.ts
@@ -17,12 +17,13 @@ const urlValidatorRegex =
   /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 
 @Component({
-  selector: 'knowledge-owl-widget',
-  template: '',
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  exportAs: 'knowledgeOwlWidget',
-  providers: [],
+    selector: 'knowledge-owl-widget',
+    template: '',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    exportAs: 'knowledgeOwlWidget',
+    providers: [],
+    standalone: false
 })
 export class KnowledgeOwlWidget implements OnInit, AfterContentInit {
   /** Flag to check initial page location updated */
