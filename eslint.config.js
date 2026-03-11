76df4acd80528e4d79874baf07adf58dba8b6974
so-kaushal <kaushal.chauhan@scheduleonce.com>
2026-03-11 17:54:00 +0530

    chore: update changelog format and fix eslint config formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
 // @ts-check
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
 
 module.exports = tseslint.config(
   {
-    ignores: ["projects/**/*"],
+    ignores: ['projects/**/*'],
   },
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       ...tseslint.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
@@ -16,56 +16,56 @@ module.exports = tseslint.config(
     processor: angular.processInlineTemplates,
     languageOptions: {
       parserOptions: {
-        project: ["tsconfig.json"],
+        project: ['tsconfig.json'],
       },
     },
     rules: {
-      "@angular-eslint/component-class-suffix": "off",
-      "@angular-eslint/directive-class-suffix": "off",
-      "@angular-eslint/prefer-standalone": "off",
-      "@angular-eslint/prefer-inject": "off",
-      "@angular-eslint/directive-selector": [
-        "error",
+      '@angular-eslint/component-class-suffix': 'off',
+      '@angular-eslint/directive-class-suffix': 'off',
+      '@angular-eslint/prefer-standalone': 'off',
+      '@angular-eslint/prefer-inject': 'off',
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: ["knowledge"],
-          style: "camelCase",
+          type: 'attribute',
+          prefix: ['knowledge'],
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: ["knowledge"],
-          style: "kebab-case",
+          type: 'element',
+          prefix: ['knowledge'],
+          style: 'kebab-case',
         },
       ],
-      "@typescript-eslint/consistent-type-definitions": "error",
-      "@typescript-eslint/dot-notation": "off",
-      "@typescript-eslint/no-floating-promises": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/member-ordering": "off",
-      "@typescript-eslint/explicit-member-accessibility": [
-        "off",
+      '@typescript-eslint/consistent-type-definitions': 'error',
+      '@typescript-eslint/dot-notation': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/member-ordering': 'off',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'off',
         {
-          accessibility: "explicit",
+          accessibility: 'explicit',
         },
       ],
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-use-before-define": "off",
-      "brace-style": ["error", "1tbs"],
-      complexity: "error",
-      "id-denylist": "off",
-      "id-match": "off",
-      "no-underscore-dangle": "off",
-      "no-useless-escape": "off",
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      'brace-style': ['error', '1tbs'],
+      complexity: 'error',
+      'id-denylist': 'off',
+      'id-match': 'off',
+      'no-underscore-dangle': 'off',
+      'no-useless-escape': 'off',
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
     extends: [...angular.configs.templateRecommended],
     rules: {},
   }
